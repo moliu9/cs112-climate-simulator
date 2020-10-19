61f58d2dddbbf54ce21d995cdc99f4205f4598e7
moliu9 <mo_liu@brown.edu>
2020-10-18 20:14:47 -0400

    design check

--- README.txt
+++ README.txt
@@ -3,7 +3,15 @@ Design Check 10/21/2020: Climate Simulator
 1)  What state will the simulator need to track?
     Be specific about the data structures you will use to track each piece of state.
 
+    We need to keep track of the present year (updated as a float: self.year), the global BAd amount (updated as a
+    float: self.BAd_ppm), the countries and their temperatures (a list with a dictionary in the form of
+    {'name': 'country name', 'temperature': 'country temperature'} for each country).
+
 2)  Why does each policy’s emit method need to take in the same arguments?
     Think about the implementation of the simulator’s advance_year method.
 
+    Because we will be calling the emit method in a for loop for each country.
+
 3)  What arguments should the emit methods take?
+
+    emit(self, baseline: float, threshold: float, increment: float)
--- simulator.py
+++ simulator.py
@@ -6,6 +6,8 @@ class Simulator:
         self.max_temperature = 100
         self.country_names = []
         self.policies = {}
+        # initialize BAd level - how to do this?
+        self.BAd_ppm = 150
         
     def add_country(self, name: str, policy):
         """Adds a country and its policy to the simulation"""
@@ -14,7 +16,19 @@ class Simulator:
         
     def advance_year(self):
         """Advances the current year, updating country and global emissions and temperatures"""
-        self.year += 1
+
+        # only implement if temp < max temp
+        if temperature > max_temperature for any country in self.policies:
+            print('This is a catastrophe!')
+        else:
+            # advance a year
+            self.year += 1
+            # reduce BAd by 1ppm (min 150 ppm)
+            self.BAd_ppm -= 1
+            # calculate emission for each country
+            # calculate total BAd emission
+            total = sum(emission from all countries)
+            # calculate temperature for each country
 
     def report(self):
         """Generates a report for use in the display"""
