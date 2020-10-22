0ebce900a191499e407dfe1725da3c349dc0ce4d
jlopez28 <71867207+jlopez28@users.noreply.github.com>
2020-10-22 01:13:08 -0400

    jhm
    hjm

--- policies.py
+++ policies.py
@@ -1,31 +1,51 @@
 class Baseline:
     """Emits at a constant rate"""
     def __init__(self, baseline: float):
-        pass
+        self.baseline = baseline
 
-    def emit(self, baseline: float, threshold: float, increment: float):
-        self.emission[self] += baseline
+    def emit(self, baseline: float, threshold: float, increment: float, temperature : list, emission: dict):
+        emission[self] += baseline
     
 class Reducing:
     """Reduces emissions every year"""
     def __init__(self, baseline: float, increment: float):
-        pass
+        self.baseline = baseline
+        self.increment = increment
 
-    def emit(self, baseline: float, threshold: float, increment: float):
-        pass
+    def emit(self, baseline: float, threshold: float, increment: float, temperature : list, emission: dict):
+        if emission[self] > 0:
+          emission[self] += (baseline - increment)
 
 class TemperaturePanic:
     """Emits at a constant rate until a temp threshold is reached"""
     def __init__(self, baseline: float, threshold: float, increment: float):
-        pass
+        self.baseline = baseline
+        self.threshold = threshold
+        self.increment = increment
 
-    def emit(self, baseline: float, threshold: float, increment: float):
-        pass
+
+
+
+    def emitemit(self, baseline: float, threshold: float, increment: float, temperature : list, emission: dict):
+        if temperature >= threshold:
+            if emission[self] > 0:
+                emission[self] += (baseline - increment)
+        else:
+            emission[self] += baseline
+
 
 class NeighborAverage:
     """Adjusts emissions towards its neighbors' average"""
     def __init__(self, baseline: float, increment: float):
-        pass
+        self.baseline = baseline
+        self.increment = increment
 
-    def emit(self, baseline: float, threshold: float, increment: float): # pass in
-        pass
+    def emit(self, baseline: float, threshold: float, increment: float, temperature: list, emission: dict):
+        avg_temp = (sum(temperature) / len(temperature))
+        if avg_temp <= emission[self]:
+            emission[self] += (baseline - increment)
+        else:
+            emission[self] += baseline
+
+
+
--- simulator.py
+++ simulator.py
@@ -9,8 +9,9 @@ class Simulator:
         # initialize BAd level of 150ppm
         self.BAd = 150
         # initialize the emission data
-        self.emission = {'country name': 'emission (ppm)'}
-        self.temperatures = []
+        self.emission = {}  # do we need this?
+        self.temperatures = []  # do we need this?#
+        self.report_data = []
         
     def add_country(self, name: str, policy):
         """Adds a country and its policy to the simulation"""
@@ -19,28 +20,29 @@ class Simulator:
         
     def advance_year(self):
         """Advances the current year, updating country and global emissions and temperatures"""
-
         # advance a year
         self.year += 1
         # reduce BAd by 1ppm (min 150 ppm)
-        self.BAd -= 1
-        # calculate emission for each country
-        # store emission data in __init__
-        # calculate global BAd amount
-        total = sum(emission from all countries)
-        # calculate temperature for each country
-        self.temepratures[0]['name'] = 'Omelas'
-        self.temepratures[0]['temperature'] = '20'
-        # halts entire process if temperature = max_temperature (stop)
-            fst_country_temp =  (self.BAd_ppm / 5)
-            for country in self.country_names:
-              new_temp = fst_country_temp  + (self.policies.index(country) * 5)
-              report_data.append[{'name': country, 'temperature': new_temp}]
-            return report_data
-
+        if self.BAd > 150:
+            self.BAd -= 1
+        fst_country_temp = (self.BAd_ppm / 5)
+        for country in self.country_names:
+            # calculate emission for each country
+            # store emission data in __init__
+            # calculate temperature for each country
+            new_temp = fst_country_temp + (self.country_names.index(country) * 5)
+            self.temperatures.append(new_temp)
+            self.emission[country] = country.emit(baseline, threshold, increment, self.temperatures, self.emission)
+            self.BAd += self.emission[country]
+            # halts entire process if temperature = max_temperature (stop)
+            # base temp
+            if new_temp >= self.max_temperature:
+                print("This is a catastrophic  event!")
+            else:
+                report_data.append[{'name': country, 'temperature': new_temp}]
+        return report_data
 
     def report(self):
         """Generates a report for use in the display"""
         # store data in the __init__ call on the list of hashtables
-        return [{'name': 'Omelas', 'temperature': 20},
-                {'name': 'Chalion', 'temperature': 85}]
+        return self.report_data
