775cfe887dca755cb4e1781e4e615e163f926d7c
jlopez28 <71867207+jlopez28@users.noreply.github.com>
2020-10-25 15:56:26 -0400

    Update policies.py

--- policies.py
+++ policies.py
@@ -1,47 +1,49 @@
 class Baseline:
     """Emits at a constant rate"""
     def __init__(self, baseline: float):
-        self.baseline = baseline
+        baseline[self] = baseline
 
     def emit(self, baseline: float, threshold: float, increment: float, emission: dict, temperature : dict):
-        emission[self] += baseline
+       return baseline[self]
 
 
 class Reducing:
     """Reduces emissions every year"""
     def __init__(self, baseline: float, increment: float):
-        self.baseline = baseline
+        baseline[self] = baseline
         self.increment = increment
 
     def emit(self, baseline: float, threshold: float, increment: float, emission: dict, temperature : dict):
         if emission[self] > 0:
-            emission[self] += (baseline - increment)
+            emission[self] -= increment
+        return emission[self]
 
 
 class TemperaturePanic:
     """Emits at a constant rate until a temp threshold is reached"""
     def __init__(self, baseline: float, threshold: float, increment: float):
-        self.baseline = baseline
+        baseline[self] = baseline
         self.threshold = threshold
         self.increment = increment
 
     def emit(self, baseline: float, threshold: float, increment: float, emission: dict, temperature: dict):
         if temperature >= threshold:
             if emission[self] > 0:
-                emission[self] += (baseline - increment)
-        else:
-            emission[self] += baseline
+                emission[self] -= increment
+
+        return emission[self]
 
 
 class NeighborAverage:
     """Adjusts emissions towards its neighbors' average"""
     def __init__(self, baseline: float, increment: float):
-        self.baseline = baseline
+        baseline[self] = baseline
         self.increment = increment
 
     def emit(self, baseline: float, threshold: float, increment: float, temperature: list, emission: dict):
-        avg_temp = (sum(temperature) / len(temperature))
-        if avg_temp <= emission[self]:
-            emission[self] += (baseline - increment)
-        else:
-            emission[self] += baseline
+            avg_temp = (sum(temperature) / len(temperature))
+            if avg_temp <= emission[self]:
+                emission[self] -= increment
+            else:
+                emission[self] += increment
+            return emission[self]
