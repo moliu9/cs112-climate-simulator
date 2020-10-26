2ebbc102b87473ae11f5618743ebd0f03b3b80ae
moliu9 <mo_liu@brown.edu>
2020-10-25 21:02:17 -0400

    change

--- policies.py
+++ policies.py
@@ -1,10 +1,10 @@
 class Baseline:
     """Emits at a constant rate"""
     def __init__(self, baseline: float):
-        self.emission = baseline
+        self.emission = baseline  # set baseline as the initial emission property
 
     def emit(self, own_temperature, neighbor_average):
-        return self.emission
+        return self.emission  # emit the same amount
 
 
 class Reducing:
@@ -14,9 +14,9 @@ class Reducing:
         self.increment = increment
 
     def emit(self, own_temperature, neighbor_average):
-        if self.emission >= 0:
-            self.emission -= self.increment
-            if self.emission < 0:
+        if self.emission >= 0:  # if the emission is equal to or greater than zero
+            self.emission -= self.increment  # decrease by set increment
+            if self.emission < 0:  # if the emission goes below zero
                 self.emission = 0
         return self.emission
 
@@ -29,10 +29,10 @@ class TemperaturePanic:
         self.increment = increment
 
     def emit(self, own_temperature, neighbor_average):
-        if own_temperature >= self.threshold:
-            if self.emission > 0:
-                self.emission -= self.increment
-                if self.emission < 0:
+        if own_temperature >= self.threshold:  # if the temperature is higher than threshold
+            if self.emission > 0:  # and the emission is more than zero
+                self.emission -= self.increment  # decrease emission by increment
+                if self.emission < 0:  # if the emission goes below zero
                     self.emission = 0
         return self.emission
 
@@ -44,11 +44,11 @@ class NeighborAverage:
         self.increment = increment
 
     def emit(self, own_temperature, neighbor_average):
-        if self.emission > neighbor_average:
-            self.emission -= self.increment
-            if self.emission < 0:
+        if self.emission > neighbor_average:  # if the emission is higher than neighbors' average
+            self.emission -= self.increment  # decrease emission by increment
+            if self.emission < 0:  # if the emission goes below zero
                 self.emission = 0
         else:
-            self.emission =+ self.increment
+            self.emission =+ self.increment  # if the emission is lower than or equal to neighbors' average
         return self.emission
 
--- simulator.py
+++ simulator.py
@@ -1,6 +1,3 @@
-from policies import *
-
-
 class Simulator:
     """Simulates the climate of Branlex"""
     
@@ -9,12 +6,9 @@ class Simulator:
         self.max_temperature = 100
         self.country_names = []
         self.policies = {}
-        # initialize BAd level of 150ppm
-        self.BAd = 150
-        # initialize the emission data
-        self.emission = {}
-        self.temperatures = {}
-        self.report_data = []
+        self.BAd = 150  # initialize BAd level of 150ppm
+        self.emission = {}  # initialize the emission data
+        self.temperatures = {}  # initialize the temperature data
         
     def add_country(self, name: str, policy):
         """Adds a country and its policy to the simulation"""
@@ -22,31 +16,33 @@ class Simulator:
         self.policies[name] = policy
 
     def find_neighbor_average(self, i, number_of_countries):
+        """"Helper function that calculates the neighboring countries' average emission """
         average = 0
-        if i == 0:
-            average = self.emission[self.country_names[i+1]]
-        elif i == number_of_countries - 1:
-            average = self.emission[self.country_names[i-1]]
-        else:
+        if i == 0:  # the north-most country (has no northern neighbor)
+            average = self.emission[self.country_names[i+1]]  # average equals to its southern neighbor's emission
+        elif i == number_of_countries - 1:  # the south-most country (has no southern neighbor)
+            average = self.emission[self.country_names[i-1]]  # average equals to its northern neighbor's emission
+        else:  # countries in the middle
             average = (self.emission[self.country_names[i-1]] + self.emission[self.country_names[i+1]]) / 2
         return average
 
     def advance_year(self):
         """Advances the current year, updating country and global emissions and temperatures"""
-        # advance a year
-        self.year += 1
-        # reduce BAd by 1ppm (min 150 ppm)
+        self.year += 1  # advance a year
         if self.BAd > 150:
-            self.BAd -= 1
+            self.BAd -= 1  # reduce BAd by 1ppm (min 150 ppm)
+
+        # calculate emission for each country using find_neighbor_average
         for i, country in enumerate(self.country_names):
-            # calculate emission for each country; store emission data in self.emission dictionary
-            self.emission[country] = \
-                self.policies[country].emit(self.temperatures[country],
-                                            self.find_neighbor_average(i, len(self.country_names)))
+            # store emission data in self.emission dictionary
+            self.emission[country] = self.policies[country].emit(self.temperatures[country],
+                                                                 self.find_neighbor_average(i, len(self.country_names)))
+            # add every country's emission to the total global emission
             self.BAd += self.emission[country]
+
         # calculate temperature for the north-most country
         base_temp = (self.BAd / 5)
-        # calculate the temperatures for the rest of the countries
+        # calculate the temperatures for the rest of the countries using temperature of north-most country
         for i, country in enumerate(self.country_names):
             self.temperatures[country] = base_temp + (self.country_names[i] * 5)
         # halts entire process if temperature = max_temperature (stop)
@@ -56,6 +52,5 @@ class Simulator:
 
     def report(self):
         """Generates a report for use in the display"""
-        # store data in the __init__ call on the list of dictionaries
         return [{'name': country_name, 'temperature': country_temperature} for country_name, country_temperature
                 in self.temperatures.items()]
